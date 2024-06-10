9e1c2b3df364828433adb1ef7b3b18ad84ea6c6c
asus test <msa-99@outlook.com>
2024-06-10 23:43:13 +0600

    vector basic txt

--- vectorIntro.cpp
+++ vectorIntro.cpp
@@ -1,8 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    //vector ekti dynamic array, za zekuno data type niye kaj korte pare. vector stl er ekti class and vector data type er array.
+    //vector ekti dynamic array library, za zekuno data type er array niye kaj korte pare. vector STL theke ase.
     //string ekti array, za char data type niye kaj kore. kintu vector array er datatype dynamic.
     //vector er initialize direct and constructor call kore kora zay.
+    // vector<int> arr(5) //ini by constructor.
+    //vector er iterator and built in function ache.
+    //vector dynamically size bariye nite pare. vector array initialize e size declare na korleo hoy.
     return 0;
 }
