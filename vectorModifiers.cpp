7316b5580f127f33778a0355a2aee5a190fed7ce
asus test <msa-99@outlook.com>
2024-06-11 01:53:54 +0600

    vector capacity func

--- vectorModifiers.cpp
+++ vectorModifiers.cpp
@@ -10,8 +10,13 @@ int main(){
     v.insert(v.begin()+1, 10); //ekhane update v er 1 number index e 10 notun add kora hoyeche.
     vector<int> b={23,55,90,12};
     v.insert(v.begin()+2, b.begin(), b.begin()+2); //ze array theke copy hobe sei array er ze index deya hobe tar ag porzonto copy hobe. ze array te copy hobe sei array er ze index deya hobe sei index theke copy hobe: v.begin()+2 v array er 2 no index theke copy howya value pawa zabe. kintu v.end() dile shesh index er valu'r por copy valu bosbe.
-    for(int val:v){
-        cout<<val<<" ";
-    }
+    // for(int val:v){
+    //     cout<<val<<" ";
+    // }
+     vector<int>a={1,3,3,4,5,3,3,3,3,3,45,6};
+     replace(a.begin()+1, a.begin()+8,3,10); //O(N) ekhane replace korar somoy ze index dibo sei index theke replace suru hobe and shesh index er ag porzonto replace hobe. ekhane a.begin()+8 deya hoyeche tai 7 index porzonto replace hobe
+     for(int vc:a){
+        cout<<vc<<" ";
+     }
     return 0;
 }
