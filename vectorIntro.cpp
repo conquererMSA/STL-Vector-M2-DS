5f748beaac32e4f6e6253ea685c200257d7ca7c1
asus test <msa-99@outlook.com>
2024-06-10 23:24:25 +0600

    vector basic txt

--- vectorIntro.cpp
+++ vectorIntro.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    //vector ekti dynamic array, za zekuno data type niye kaj korte pare. vector stl er ekti class and vector data type er array.
+    //string ekti array, za char data type niye kaj kore. kintu vector array er datatype dynamic.
+    //vector er initialize direct and constructor call kore kora zay.
     return 0;
 }
